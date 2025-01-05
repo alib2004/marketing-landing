26b06647691c5d63a25d6f568efdc13ed1814d97
alib2004 <aliw.b2004@gmail.com>
2025-01-05 16:34:11 +0330

    create another elements

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-let sliders = document.querySelector('.slider')
-const swiper = new Swiper(sliders,{
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-})
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+let sliders = document.querySelector(".slider");
+const swiper = new Swiper(sliders, {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+let buyers = document.querySelector(".buyers-swiper");
+const viewswiper = new Swiper(buyers, {
+  pagination: {
+    el: ".buyers-pagination",
+    dynamicBullets: true,
+  },
+});
